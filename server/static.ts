f5deb0ae443ed9a830d6a6f14ab16810f6588132
Paul Fleury <41485648+paulfxyz@users.noreply.github.com>
2026-03-29 10:35:58 +0000

    chore: bump all version strings to 4.0.0

--- server/static.ts
+++ server/static.ts
@@ -5,7 +5,7 @@ import path from "path";
 /**
  * @file server/static.ts
  * @author Paul Fleury <hello@paulfleury.com>
- * @version 3.5.9
+ * @version 4.0.0
  *
  * Static file serving + SPA hash-route redirects.
  *
